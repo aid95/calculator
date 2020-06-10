e32f6ac2b3289ef6913a166613a7eec5b69b688e
imgomi <webmaster@mail.gomi.land>
2020-06-10 19:29:50 +0900

    CalculatorTest.java에 Javadoc 추가

--- src/test/java/land/gomi/calculator/CalculatorTest.java
+++ src/test/java/land/gomi/calculator/CalculatorTest.java
@@ -3,14 +3,23 @@ package land.gomi.calculator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
+/**
+ * CalculatorTest class
+ */
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
+    /**
+     * Sum
+     */
     @Test
     public void sum() {
         assertEquals(5, calculator.sum(2, 3));
     }
 
+    /**
+     * Sub
+     */
     @Test
     public void sub() {
         assertEquals(1, calculator.sub(3, 2));
