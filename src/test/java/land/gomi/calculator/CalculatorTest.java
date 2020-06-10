c474af516793630cbb414330716d20037573ac57
imgomi <webmaster@mail.gomi.land>
2020-06-10 16:56:02 +0900

    FIX : NullPointException 오류 제거

--- src/test/java/land/gomi/calculator/CalculatorTest.java
+++ src/test/java/land/gomi/calculator/CalculatorTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-    private Calculator calculator;
+    private Calculator calculator = new Calculator();
 
     @Test
     public void sum() {
