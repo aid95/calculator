823fc787ae7580023444bec5bbf341a0202079fa
imgomi <webmaster@mail.gomi.land>
2020-06-10 16:51:59 +0900

    jacoco test

--- src/main/java/land/gomi/calculator/Calculator.java
+++ src/main/java/land/gomi/calculator/Calculator.java
@@ -7,4 +7,8 @@ public class Calculator {
     int sum(int a, int b) {
         return a + b;
     }
+
+    int sub(int a, int b) {
+        return a - b;
+    }
 }
--- src/test/java/land/gomi/calculator/CalculatorTest.java
+++ src/test/java/land/gomi/calculator/CalculatorTest.java
@@ -4,10 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-    private Calculator calculator = new Calculator();
+    private Calculator calculator;
 
     @Test
     public void sum() {
         assertEquals(5, calculator.sum(2, 3));
+    }
+
+    @Test
+    public void sub() {
+        assertEquals(1, calculator.sub(3, 2));
     }
 }
